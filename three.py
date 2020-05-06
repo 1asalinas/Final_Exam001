d8e2e38d303bd2e51ab2d484f87702874aa3a3e1
1asalinas <42749574+1asalinas@users.noreply.github.com>
2020-05-06 07:40:52 -0500

    Update three.py

--- three.py
+++ three.py
@@ -11,11 +11,23 @@ def tell_story():
     c = Gorilla()
     # Generate  as many other objects as you want
 
-
-
+    d = Beetle()
+    e = Chipmunk()
+    f = Snake()
+    g = Cobra()
+    h = Goat()
+    i = Sheep()
+    j = Donkey()
+    k = Pig()
 
     # Have objects interact
 
+    b.eat(d)
+    b.eat(f)
+    b.eat(j)
+    b.eat(a)
+    b.eat(c)
+    b.eat(k)
 
     # replace the replace_me_object
     replace_me_object = b
@@ -23,14 +35,16 @@ def tell_story():
     return final_phrase
 
 
-
 class Animal:
     def __init__(self):
         self.phrase = ""
+
     def eat(self, other):
         self.phrase += str(other.phrase)
+
     def talk(self):
         return self.phrase
+
 
 class Beetle(Animal):
     def __init__(self):
@@ -43,67 +57,83 @@ class Bird(Animal):
         super().__init__()
         self.phrase = "Tweet"
 
+
 class Dragon(Animal):
     def __init__(self):
         super().__init__()
+
     def eat(self, other):
         self.phrase = str(other.phrase[::-1])
+
     def talk(self):
         return self.phrase[0::2]
+
 
 class Gorilla(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "123456789"
+
     def beat(self, other1, other2):
         self.phrase = str(other1.phrase) + " " + str(other2.phrase)
+
 
 class Chipmunk(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "Munchd"
-    def eat(self,other):
+
+    def eat(self, other):
         self.phrase = str(other.phrase[0])
+
 
 class Snake(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "Hiss"
-    def eat(self,other):
+
+    def eat(self, other):
         self.phrase = str(other.phrase).toLower()
+
 
 class Cobra(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "Snorflek"
-    def eat(self,other):
+
+    def eat(self, other):
         self.phrase = str(other.phrase).toUpper()
+
 
 class Goat(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "Brae"
-    def eat(self,other):
-        self.phrase = str(other.phrase)+str(",")
+
+    def eat(self, other):
+        self.phrase = str(other.phrase) + str(",")
+
 
 class Sheep(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "GypJ"
 
+
 class Donkey(Animal):
     def __init__(self):
         super().__init__()
         self.phrase = "vozx"
-    def eat(self,other):
-        self.phrase = str(other.phrase)+str("!")
+
+    def eat(self, other):
+        self.phrase = str(other.phrase) + str("!")
+
 
 class Pig(Animal):
     def __init__(self):
         super().__init__()
-        self.phrase ="eduj"
-
+        self.phrase = "eduj"
 
 
 if __name__ == "__main__":
-    print(tell_story()=="Sphinx of black quartz, judge my vow!")
+    print(tell_story() == "Sphinx of black quartz, judge my vow!")
